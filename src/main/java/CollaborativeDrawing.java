ece63126ce56e266c66621af54dc4d2f25161362
Kristian Angelin <kran1800@student.miun.se>
2020-12-08 11:48:32 +0100

    - First commit for project.

---
+++ src/main/java/CollaborativeDrawing.java
@@ -0,0 +1,16 @@
+import io.reactivex.Observable;
+import io.reactivex.rxjavafx.observables.JavaFxObservable;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CollaborativeDrawing extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        System.out.println("First test!");
+    }
+}
